c6d00a75a610bc37776721392f9cca146b7f8222
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-03-22 14:46:44 +0000

    Initial commit

---
+++ src/main/java/com/fve/springbootmavenstartertemplate/SpringbootmavenstartertemplateApplication.java
@@ -0,0 +1,13 @@
+package com.fve.springbootmavenstartertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmavenstartertemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmavenstartertemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fve/springbootmavenstartertemplate/SpringbootmavenstartertemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fve.springbootmavenstartertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmavenstartertemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
